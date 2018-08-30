5550ad1cdb6a19e96c4f012cbb9a9e413736b485
Jasper Francisco <jasper.francisco@gmail.com>
2018-08-30 20:38:34 +0800

    Make guide modal interactive

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import $ from 'jquery';
 import PTimer from './js/PTimer';
+import PEvents from './js/PEvents';
 import Todo from './js/Todo';
 
 $(document).ready(function() {
   PTimer.init();
+  PEvents.init();
   Todo.init();
 });
