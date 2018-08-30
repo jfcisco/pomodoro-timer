c9b7d9d1bed9a9be0f80d3ae01a63b8f741f8b7f
Jasper Francisco <jasper.francisco@gmail.com>
2018-08-30 21:16:18 +0800

    Handle favicon loading in Webpack

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import PTimer from './js/PTimer';
 import PEvents from './js/PEvents';
 import Todo from './js/Todo';
 
+// favicons
+require('./favicons/favicon-16x16.png');
+require('./favicons/favicon-32x32.png');
+
 $(document).ready(function() {
   PTimer.init();
   PEvents.init();
