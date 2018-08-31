048fc4ef3ef220dcd195aaa886a64f8eb14cc434
Jasper Francisco <jasper.francisco@gmail.com>
2018-08-31 13:32:57 +0800

    Use webpack-dev-server for development

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import $ from 'jquery';
-import PTimer from './js/PTimer';
-import PEvents from './js/PEvents';
+import Timer from './js/Timer';
+import Pomodoro from './js/Pomodoro';
 import Todo from './js/Todo';
 
 // favicons
 require('./favicons/favicon-16x16.png');
 require('./favicons/favicon-32x32.png');
 
+// css
+require('./style.css');
+
+
 $(document).ready(function() {
-  PTimer.init();
-  PEvents.init();
+  Timer.init();
+  Events.init();
   Todo.init();
 });
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
 const path = require('path');
-const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './src/index.js',
@@ -10,7 +10,7 @@ module.exports = {
   module: {
 	  rules: [
 	  {
-		test: /\.(ico|png)$/,
+		test: /\.(ico|png|css)$/,
 		use: [
 		{
 			loader: "file-loader",
@@ -23,10 +23,8 @@ module.exports = {
 	]
   },
   plugins: [
-    new BrowserSyncPlugin({
-      host: 'localhost',
-      port: 3000,
-      server: "dist"
-    })
+    new HtmlWebpackPlugin({
+		template: 'src/index.html',
+	})
   ]
 }
